e30740dc9ac9007d3b36918166b6b4d5d8251932
Sietra <canercanturk2004@gmail.com>
2022-08-02 00:24:31 +0300

    Modified target locator to atack closest enemy

--- Assets/Scripts/TargetLocator.cs
+++ Assets/Scripts/TargetLocator.cs
@@ -6,18 +6,31 @@ public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
     Transform target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
 
-    // Update is called once per frame
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
     }
 
+    void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach (Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+        target = closestTarget;
+    }
     void AimWeapon()
     {
         weapon.LookAt(target);
