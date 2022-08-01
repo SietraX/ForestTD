11bfbf991f1c4930a29e5ed1f70e6711626a16dd
Sietra <canercanturk2004@gmail.com>
2022-08-02 00:30:30 +0300

    Modified target locator script for particle system

--- Assets/Scripts/TargetLocator.cs
+++ Assets/Scripts/TargetLocator.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    [SerializeField] float range = 15f;
+    [SerializeField] ParticleSystem projectileParticles;
     Transform target;
 
     void Update()
@@ -33,6 +35,19 @@ public class TargetLocator : MonoBehaviour
     }
     void AimWeapon()
     {
+        float targetDistance = Vector3.Distance(transform.position, target.position);
         weapon.LookAt(target);
+
+        if (targetDistance < range)
+        {
+            Attack(true);
+        }
+        else { Attack(false); }
+    }
+
+    void Attack(bool isActive)
+    {
+        var emissionModule = projectileParticles.emission;
+        emissionModule.enabled = isActive;
     }
 }
